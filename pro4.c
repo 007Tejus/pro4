d7a14226e7d00a78b7cb7191e81124c0cb939fe0
007Tejus <161346725+007Tejus@users.noreply.github.com>
2024-07-17 23:14:31 +0530

    pro4.c

--- pro4.c
+++ pro4.c
@@ -1,40 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-void insertion_sort(long a[], int n) {
-    int i, j;
-    long v = 0;
-    for (i = 1; i < n; i++) {
-        v = a[i];
-        j = i - 1;
-        while (j >= 0 && a[j] > v) {
-            a[j + 1] = a[j];
-            j = j - 1;
+typedef struct
+{
+    int u, v, cost;
+} edge;
+int find(int v, int parent[])
+{
+    while (parent[v] != v)
+        v = parent[v];
+    return v;
+}
+void union_ij(int i, int j, int parent[])
+{
+    parent[j] = i;
+}
+void kruskal(int n, edge e[], int m)
+{
+    int count = 0, k = 0, sum = 0, t[10][2], parent[10];
+    edge temp;
+
+    for (int i = 0; i < m - 1; i++)
+        for (int j = 0; j < m - 1 - i; j++)
+            if (e[j].cost > e[j + 1].cost)
+            {
+                temp = e[j];
+                e[j] = e[j + 1];
+                e[j + 1] = temp;
+            }
+
+    for (int i = 0; i < n; i++)
+        parent[i] = i;
+    for (int i = 0; count != n - 1 && i < m; i++)
+    {
+        int u = e[i].u, v = e[i].v;
+        int root_u = find(u, parent), root_v = find(v, parent);
+
+        if (root_u != root_v)
+        {
+            t[k][0] = u;
+            t[k][1] = v;
+            k++;
+            count++;
+            sum += e[i].cost;
+            union_ij(root_u, root_v, parent);
         }
-        a[j + 1] = v;
     }
-    printf("sorted numbers are:\n");
-    for (i = 0; i < n; i++)
-        printf("%ld\n", a[i]);
+
+    if (count == n - 1)
+    {
+        printf("Spanning tree exists\nThe spanning tree is:\n");
+        for (int i = 0; i < n - 1; i++)
+            printf("%d  %d\t", t[i][0], t[i][1]);
+        printf("\nCost of the spanning tree: %d\n", sum);
+    }
+    else
+    {
+        printf("\nSpanning tree does not exist\n");
+    }
 }
 
-int main() {
-    clock_t starttime, endtime;
-    starttime = clock();
-    double clk;
-    int i, n;
-    long a[10];
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-        a[i] = 9000000000 + (rand() % 99999999) + 1;
-    printf("\nphone numbers are:\n");
-    for (i = 0; i < n; i++)
-        printf("%ld\n", a[i]);
-    insertion_sort(a, n);
-    endtime = clock();
-    clk = (double)(endtime - starttime) / CLOCKS_PER_SEC;
-    printf("\nTHE RUN TIME IS :%f\n", clk);
+int main()
+{
+    int n, m;
+    edge e[20];
+
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &n, &m);
+    printf("Enter the edge list (u v cost):\n");
+    for (int i = 0; i < m; i++)
+        scanf("%d %d %d", &e[i].u, &e[i].v, &e[i].cost);
+    kruskal(n, e, m);
     return 0;
 }
